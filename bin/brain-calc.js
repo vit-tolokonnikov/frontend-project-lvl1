ba5d82264310bfc6b39ae88e00b2c3190ee3f01c
vitaliy <vitaly.tolokonnikov@gmail.com>
2024-05-29 07:36:09 +0700

    Sixth step.

---
+++ bin/brain-calc.js
@@ -0,0 +1,7 @@
+import askName from "../src/cli.js";
+import { calc } from "../src/games.js";
+
+console.log("Welcome to the Brain Games!");
+const name = askName();
+
+calc(name);
--- src/games.js
+++ src/games.js
@@ -1,32 +1,82 @@
 import readlineSync from "readline-sync";
 
 const evenOdd = (n) => (n % 2 === 0 ? "yes" : "no");
+const getCalcExpretion = (name) => {
+    let result = '';
+    const basicOperation = ['+', '-', '*'];
+    const randomIndex = Math.floor(Math.random() * (basicOperation.length - 1));
+    const operationElement = basicOperation[randomIndex];
 
-export const even = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const num1 = Math.ceil(Math.random() * 100);
+    const num2 = Math.ceil(Math.random() * 100);
 
-  let i = 0;
-
-  while (i < 3) {
-    const curNumber = Math.ceil(Math.random() * 100);
-    const scriptAnswer = evenOdd(curNumber);
-
-    const answer = readlineSync.question(
-      `Question: ${curNumber}\nYour answer: `,
-    );
-
-    if (scriptAnswer === answer) {
-      console.log("Correct!");
-    } else {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${scriptAnswer}'.\nLet's try again, ${name}!`,
-      );
-      break;
+    if (operationElement === '+') {
+        result = Number(num1) + Number(num2);
+    } else if (operationElement === '-') {
+        result = Number(num1) - Number(num2);
+    } else if (operationElement === '*') {
+        result = Number(num1) * Number(num2);
     }
 
-    i += 1;
-  }
-  if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+    const answer = readlineSync.question(
+        `Question: ${num1} ${operationElement} ${num2}\nYour answer: `,
+    );
+
+    if (Number(result) === Number(answer)) {
+        console.log("Correct!");
+        return true;
+    } else {
+        console.log(
+            `'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${name}!`,
+        );
+        return false;
+    }
+}
+
+export const even = (name) => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    let i = 0;
+
+    while (i < 3) {
+        const curNumber = Math.ceil(Math.random() * 100);
+        const scriptAnswer = evenOdd(curNumber);
+
+        const answer = readlineSync.question(
+            `Question: ${curNumber}\nYour answer: `,
+        );
+
+        if (scriptAnswer === answer) {
+            console.log("Correct!");
+        } else {
+            console.log(
+                `'${answer}' is wrong answer ;(. Correct answer was '${scriptAnswer}'.\nLet's try again, ${name}!`,
+            );
+            break;
+        }
+
+        i += 1;
+    }
+    if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+    }
+};
+
+export const calc = (name) => {
+    console.log('What is the result of the expression?');
+
+    let i = 0;
+
+    while (i < 3) {
+        const result = getCalcExpretion(name);
+
+        if (!result) {
+            break;
+        }
+
+        i += 1;
+    }
+    if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+    }
 };
