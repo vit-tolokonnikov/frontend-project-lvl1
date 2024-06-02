44cd4b17e6cec8347449ca541e3351ecc5ccb273
vitaliy <vitaly.tolokonnikov@gmail.com>
2024-06-03 00:19:32 +0700

    Ninth step. Use prettier, add descriptions

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+
+/**
+ * Описание скрипта brain-even
+ * Этот скрипт проверяет, является ли число чётным.
+ */
 import askName from "../src/cli.js";
 import { even } from "../src/games.js";
 
